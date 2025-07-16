ae114d280c5e4b2db0fe9b88e14677e1be6afd67
RonenTGreat <ronenh53@gmail.com>
2025-07-16 16:36:13 +0000

    created function to edit, update and deleted products

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,7 @@ Route::get('/products/{id}', [ProductController::class, 'show'])->name("product.
 Route::get("/admin", [AdminHomeController::class, 'index'])->name("admin.home.index");
 Route::get("/admin/products", [AdminProductController::class, 'index'])->name("admin.product.index");
 Route::post("/admin/products/store", [AdminProductController::class, 'store'])->name("admin.product.store");
+Route::delete("/admin/products/{id}/delete", [AdminProductController::class, 'delete'])->name("admin.product.delete");
+Route::get("/admin/products/{id}/edit", [AdminProductController::class, 'edit'])->name("admin.product.edit");
+Route::put("/admin/products/{id}/update", [AdminProductController::class, 'update'])->name("admin.product.update");
 
