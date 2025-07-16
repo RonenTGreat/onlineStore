f1faad5877e9ab3fd64e137c3c6703763bd0b4c5
RonenTGreat <ronenh53@gmail.com>
2025-07-16 17:16:07 +0000

    added ui and auth

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+
 use App\Http\Controllers\Admin\AdminHomeController;
 use App\Http\Controllers\Admin\AdminProductController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,7 +19,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+Route::get('/', [HomeController::class, 'index'])->name("home.index");
 Route::get('/about', [HomeController::class, 'about'])->name("home.about");
 Route::get('/products', [ProductController::class, 'index'])->name("product.index");
 Route::get('/products/{id}', [ProductController::class, 'show'])->name("product.show");
@@ -27,4 +30,6 @@ Route::post("/admin/products/store", [AdminProductController::class, 'store'])->
 Route::delete("/admin/products/{id}/delete", [AdminProductController::class, 'delete'])->name("admin.product.delete");
 Route::get("/admin/products/{id}/edit", [AdminProductController::class, 'edit'])->name("admin.product.edit");
 Route::put("/admin/products/{id}/update", [AdminProductController::class, 'update'])->name("admin.product.update");
+
+Auth::routes();
 
