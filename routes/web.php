321008cdc6e38c923cd7c7890d03fd03d5958ff4
RonenTGreat <ronenh53@gmail.com>
2025-07-17 10:43:55 +0000

    added admin atuh middlewar funcationality

--- routes/web.php
+++ routes/web.php
@@ -24,12 +24,14 @@ Route::get('/', [HomeController::class, 'index'])->name("home.index");
 Route::get('/about', [HomeController::class, 'about'])->name("home.about");
 Route::get('/products', [ProductController::class, 'index'])->name("product.index");
 Route::get('/products/{id}', [ProductController::class, 'show'])->name("product.show");
-Route::get("/admin", [AdminHomeController::class, 'index'])->name("admin.home.index");
-Route::get("/admin/products", [AdminProductController::class, 'index'])->name("admin.product.index");
-Route::post("/admin/products/store", [AdminProductController::class, 'store'])->name("admin.product.store");
-Route::delete("/admin/products/{id}/delete", [AdminProductController::class, 'delete'])->name("admin.product.delete");
-Route::get("/admin/products/{id}/edit", [AdminProductController::class, 'edit'])->name("admin.product.edit");
-Route::put("/admin/products/{id}/update", [AdminProductController::class, 'update'])->name("admin.product.update");
+
+Route::middleware('admin')->group(function () {
+    Route::get("/admin", [AdminHomeController::class, 'index'])->name("admin.home.index");
+    Route::get("/admin/products", [AdminProductController::class, 'index'])->name("admin.product.index");
+    Route::post("/admin/products/store", [AdminProductController::class, 'store'])->name("admin.product.store");
+    Route::delete("/admin/products/{id}/delete", [AdminProductController::class, 'delete'])->name("admin.product.delete");
+    Route::get("/admin/products/{id}/edit", [AdminProductController::class, 'edit'])->name("admin.product.edit");
+    Route::put("/admin/products/{id}/update", [AdminProductController::class, 'update'])->name("admin.product.update");
+});
 
 Auth::routes();
-
