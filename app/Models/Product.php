6c2a2e6da7dab195d3b0bfba65c56d25502ba84f
RonenTGreat <ronenh53@gmail.com>
2025-07-19 11:52:00 +0000

    added order and item tables and functionality

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Item;
 
 class Product extends Model
 {
@@ -15,6 +16,7 @@ class Product extends Model
      * $this->attributes['price'] - int - contains the product price
      * $this->attributes['created_at'] - timestamp - contains the product creation date
      * $this->attributes['updated_at'] - timestamp - contains the product update date
+     * $this->items - Item[] - contains the associated items
      */
 
     public static function sumPricesByQuantities($products, $productsInSession)
@@ -91,5 +93,18 @@ class Product extends Model
             "price" => "required|numeric|gt:0",
             'image' => 'image',
         ]);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(Item::class);
+    }
+    public function getItems()
+    {
+        return $this->items;
+    }
+    public function setItems($items)
+    {
+        $this->items = $items;
     }
 }
